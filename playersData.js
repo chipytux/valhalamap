505cf0f9c065ea5109c0e51c56e97be88323e9b6
chipytux <chipytux@users.noreply.github.com>
2020-07-30 20:45:53 -0300

    Segundo Envio

---
+++ playersData.js
@@ -0,0 +1,104 @@
+// NOTE: Please only modify player attributes below such as name, color, and visible
+// To hide a player's marker, change their 'visible' variable from 'true' to 'false'
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Changes will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var playersData = {
+  "players": [
+    {
+      "uuid": "2786db17-3f9b-41bc-8f93-da6b60389b15",
+      "name": "Player 2786d",
+      "dimensionId": 0,
+      "position": [
+        2.2879403,
+        70.62001,
+        278.98557
+      ],
+      "color": "Teal",
+      "visible": true
+    },
+    {
+      "uuid": "2ee6a842-f895-4a19-a6f3-8ddd8636f607",
+      "name": "Player 2ee6a",
+      "dimensionId": 0,
+      "position": [
+        3.6790543,
+        118.62001,
+        0.62008274
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "49620386-29b8-4d02-a614-c92ec9fba985",
+      "name": "Player 49620",
+      "dimensionId": 0,
+      "position": [
+        9168.5,
+        67.62001,
+        -3400.5
+      ],
+      "color": "DarkOrange",
+      "visible": true
+    },
+    {
+      "uuid": "6da9f6c4-b829-4a0d-8d8a-94ddbbc10d6e",
+      "name": "Player 6da9f",
+      "dimensionId": 0,
+      "position": [
+        227.52798,
+        66.62001,
+        202.34962
+      ],
+      "color": "DarkRed",
+      "visible": true
+    },
+    {
+      "uuid": "78e474f9-5787-4fb5-8aba-310aeab036cc",
+      "name": "Player 78e47",
+      "dimensionId": 0,
+      "position": [
+        -7.5225234,
+        71.62001,
+        129.00307
+      ],
+      "color": "Purple",
+      "visible": true
+    },
+    {
+      "uuid": "c6cbd964-fd22-4f76-930e-2e718f4cf851",
+      "name": "Player c6cbd",
+      "dimensionId": 0,
+      "position": [
+        143.36713,
+        65.62001,
+        315.63794
+      ],
+      "color": "DarkOrange",
+      "visible": true
+    },
+    {
+      "uuid": "da7470ee-6a81-4ede-8ec5-ee6b3482a316",
+      "name": "Player da747",
+      "dimensionId": 2,
+      "position": [
+        1269.7,
+        130.62001,
+        -10492.687
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "f9192835-ccda-449c-97ab-2c4bd48670e6",
+      "name": "Player f9192",
+      "dimensionId": 0,
+      "position": [
+        9206.146,
+        71.62001,
+        1602.8048
+      ],
+      "color": "DeepPink",
+      "visible": true
+    }
+  ]
+};
